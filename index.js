f3e647a065f62a07907f51748a5e3006c7432c9e
biso <bopoku89@gmail.com>
2024-07-11 14:47:21 -0400

    Added body-parser, root route, and views folder - Adding View Engine next

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(`-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
+
+  if (Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Expense Tracker</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
