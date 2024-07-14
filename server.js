8d52f3d4247408ee0a3fc34a0180b615cf043021
biso <bopoku89@gmail.com>
2024-07-14 00:49:42 -0400

    Added public dir, transaction page, submit button event handler

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const port = 3000;
 
 app.set("view engine", "ejs")
 
+app.use(express.static("public"))
 app.use(bodyParser.json());
+
 
 app.use((req, res, next) => {
   const time = new Date();
 
   console.log(`-----
-${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}`);
 
   if (Object.keys(req.body).length > 0) {
     console.log("Containing the data:");
@@ -22,6 +24,10 @@ ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/transactions", (req, res) => {
+  res.render("transactions");
 });
 
 app.listen(port, () => {
