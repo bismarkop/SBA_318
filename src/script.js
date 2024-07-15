c3f34d63dd27bcff7c4362b7e4cefe52b6955a96
biso <bopoku89@gmail.com>
2024-07-14 22:59:44 -0400

    Fixed null issue with name, added an SVG X button for deleting

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+const transactions = JSON.parse(sessionStorage.getItem("transactions")) || [];
 const balance = document.getElementById("balance");
 const expense = document.getElementById("expense");
 const income = document.getElementById("income");
@@ -7,7 +7,7 @@ const nameOf = document.getElementById("name");
 const amount = document.getElementById("amount");
 const date = document.getElementById("date");
 const list = document.getElementById("transactionList");
-const status = document.getElementById("status");
+const tranStatus = document.getElementById("status");
 
 let formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
@@ -16,6 +16,35 @@ let formatter = new Intl.NumberFormat("en-US", {
 });
 
 form.addEventListener("submit", addTransaction);
+
+function renderList() {
+  list.innerHTML = "";
+
+  transactions.forEach(({ id, name, amount, date, type }) => {
+    let sign = "income" === type ? 1 : -1;
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+            <p>${new Date(date).toLocaleDateString()}</p>
+            <div class = "name">
+              <h4>${name}</h4>
+            </div>
+            <div class="amount ${type}">
+              <span>${formatter.format(amount * sign)}</span>
+            </div>
+            <div class="action">
+            <svg width="150" height="100" xmlns="http://www.w3.org/2000/svg" onclick="deleteTransaction(${id})">
+              <circle cx="75" cy="50" r="20" fill="red" />
+              <text x="75" y="60" font-size="30" text-anchor="middle" fill="white">X</text>
+              Sorry, your browser does not support inline SVG.  
+            </svg> 
+      </div>
+          `;
+    list.appendChild(li);
+  });
+}
+
+renderList();
 
 function addTransaction(e) {
   e.preventDefault();
@@ -35,35 +64,16 @@ function addTransaction(e) {
   renderList();
 }
 
-function renderList() {
-  list.innerHTML = "";
+function deleteTransaction() {
+  let index = transactions.findIndex((trans) => trans.id === id);
+  transactions.splice(index, 1);
 
-  // status.textContent = "";
-  // if (transactions.length === 0) {
-  //   status.textContent = "No transactions yet.";
-  //   return;
-  // }
-
-  transactions.forEach(({ id, name, amount, date, type }) => {
-    let sign = "income" === type ? 1 : -1;
-    const li = document.createElement("li");
-
-    li.innerHTML = `
-            <p>${new Date(date).toLocaleDateString()}</p>
-            <div class = "name">
-              <h4>${name}</h4>
-            </div>
-            <div class="amount ${type}">
-              <span>${formatter.format(amount * sign)}</span>
-            </div>
-            
-          `;
-    list.appendChild(li);
-  });
+  saveTransaction();
+  renderList();
 }
 
 function saveTransaction() {
   transactions.sort((a, b) => new Date(a.date) - new Date(b.date));
 
-  localStorage.setItem("transactions", JSON.stringify(transactions));
+  sessionStorage.setItem("transactions", JSON.stringify(transactions));
 }
